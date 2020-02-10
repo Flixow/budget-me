32417150269722d15b7688b19425f9f0f9a96162
Daniel Siwek <danielsiwek95@gmail.com>
2020-02-10 16:54:08 +0100

    Translate AddTransactionForm

--- src/pages/Budget/components/AddTransactionForm/AddTransactionForm.js
+++ src/pages/Budget/components/AddTransactionForm/AddTransactionForm.js
@@ -1,11 +1,13 @@
 import React, { useMemo } from 'react';
 import { Form, Field } from 'react-final-form';
+import { useTranslation } from 'react-i18next';
 import { noop, groupBy } from 'lodash';
 import { Button } from 'components';
 
 const required = value => (value ? undefined : 'Required');
 
 const AddTransactionForm = ({ onSubmit = noop, categories, groupCategoriesBy }) => {
+  const { t } = useTranslation();
   const categoryItems = useMemo(() => groupCategoriesBy ?
     Object.entries(groupBy(categories, groupCategoriesBy))
       .map(([key, categories]) => (
@@ -26,44 +28,44 @@ const AddTransactionForm = ({ onSubmit = noop, categories, groupCategoriesBy }) 
           <Field name="description" validate={required}>
             {({ input, meta }) => (
               <div>
-                <label>Description</label>
-                <input {...input} type="text" placeholder="Description" />
-                {meta.error && meta.touched && <span>{meta.error}</span>}
+                <label>{t('Description')}</label>
+                <input {...input} type="text" placeholder={t('Description')} />
+                {meta.error && meta.touched && <span>{t(meta.error)}</span>}
               </div>
             )}
           </Field>
           <Field name="amount" validate={required} parse={value => parseFloat(value, 10)}>
             {({ input, meta }) => (
               <div>
-                <label>Amount</label>
-                <input {...input} type="number" step="0.01" placeholder="Amount" />
-                {meta.error && meta.touched && <span>{meta.error}</span>}
+                <label>{t('Amount')}</label>
+                <input {...input} type="number" step="0.01" placeholder={t('Amount')} />
+                {meta.error && meta.touched && <span>{t(meta.error)}</span>}
               </div>
             )}
           </Field>
           <Field name="categoryId" validate={required} parse={value => parseFloat(value, 10)}>
             {({ input, meta }) => (
               <div>
-                <label>Category</label>
+                <label>{t('Category')}</label>
                 <select {...input}>
                   {categoryItems}
                 </select>
-                {meta.error && meta.touched && <span>{meta.error}</span>}
+                {meta.error && meta.touched && <span>{t(meta.error)}</span>}
               </div>
             )}
           </Field>
           <Field name="date" validate={required}>
             {({ input, meta }) => (
               <div>
-                <label>Date</label>
-                <input {...input} type="date" placeholder="Last Name" />
-                {meta.error && meta.touched && <span>{meta.error}</span>}
+                <label>{t('Date')}</label>
+                <input {...input} type="date" />
+                {meta.error && meta.touched && <span>{t(meta.error)}</span>}
               </div>
             )}
           </Field>
           <div>
             <Button variant="regular" type="submit" disabled={submitting}>
-              Submit
+              {t('Submit')}
             </Button>
           </div>
         </form>
